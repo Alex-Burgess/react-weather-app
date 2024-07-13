6469805d66df18787b56316435607eb1ea415308
Alex-Burgess <9518982+Alex-Burgess@users.noreply.github.com>
2024-07-13 07:55:54 +0100

    case rename 2

--- web-app/src/main.tsx
+++ web-app/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import Root from './routes/RootNew';
+import Root from './routes/Root';
 import Tomorrow from './routes/Tomorrow';
 import ErrorPage from './ErrorPage';
 
